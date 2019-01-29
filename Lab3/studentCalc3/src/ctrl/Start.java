080b1486e7652cd9aa4369fb47f8961950240f53
Adr ADR <adrianw@my.yorku.ca>
2019-01-29 01:16:31 -0500

    added war files for lab 1 and 2, finished Lab3 and included the war file

--- Lab3/studentCalc3/src/ctrl/Start.java
+++ Lab3/studentCalc3/src/ctrl/Start.java
@@ -20,13 +20,23 @@ import model.Loan;
 @WebServlet({"/Start", "/Startup", "/Startup/*"})
 public class Start extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	Loan loanobj;
+	boolean error = false;
+	String errorMsg;
+	       
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Start() {
         super();
         // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init() throws ServletException {
+        loanobj = new Loan();
+		getServletContext().setAttribute("model", loanobj);
+	
     }
 
 	/**
@@ -37,8 +47,7 @@ public class Start extends HttpServlet {
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		response.setContentType("text/plain");
-        Loan obj = new Loan();
-		
+
 		String target = "";
 
 		
@@ -51,68 +60,75 @@ public class Start extends HttpServlet {
 			
 			
 			String principal = request.getParameter("principalone");
-			if(principal == null || principal.isEmpty())
-			{
-				principal = getServletContext().getInitParameter("principal");
-			}
-
+//			if(principal == null || principal.isEmpty())
+//			{
+//				principal = getServletContext().getInitParameter("principal");
+//			}
+//			System.out.println("777");
 			
 			String period = request.getParameter("periodone");
-			if(period == null ||period.isEmpty())
-			{
-				period = getServletContext().getInitParameter("period");
-			}
-
+//			if(period == null ||period.isEmpty())
+//			{
+//				period = getServletContext().getInitParameter("period");
+//			}
+//			System.out.println("767");
 			
 			String interest = request.getParameter("interestone");
-			if(interest == null || interest.isEmpty())
-			{
-				interest = getServletContext().getInitParameter("interest");
-			}
+//			if(interest == null || interest.isEmpty())
+//			{
+//				interest = getServletContext().getInitParameter("interest");
+//			}
 			String grace = request.getParameter("graceP");
 			boolean graceVal = false;
-			System.out.println("|" + grace + "|");
+//			System.out.println("|" + grace + "|");
 			if(grace == null)
 			{
 				graceVal = false;
 			} else {
 				graceVal = true;
 			}
+//			System.out.println("787");
 			
-			
-			double principalVal = Double.parseDouble(principal);
-			double interestVal = Double.parseDouble(interest) / 100;
-			double periodVal =  Double.parseDouble(period);
-			double fixedinterestVal = Double.parseDouble(getServletContext().getInitParameter("interest"));
-			double gracePeriodVal =  Double.parseDouble(getServletContext().getInitParameter("gracePeriod"));
+//			double principalVal = Double.parseDouble(principal);
+//			double interestVal = Double.parseDouble(interest) / 100;
+//			double periodVal =  Double.parseDouble(period);
+			String fixedinterest = getServletContext().getInitParameter("interest");
+			String gracePeriod = getServletContext().getInitParameter("gracePeriod");
 			
 			
 			//Calculations
 			double graceInterest;
-			try {
-				graceInterest = Loan.computeGraceInterest(principalVal, interestVal, fixedinterestVal, 
-						graceVal, gracePeriodVal);
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				graceInterest = 0.0;
-				
-			}
-			
 			double monthlyPayment;
 			try {
-				monthlyPayment = Loan.computePayment(principalVal, interestVal, periodVal, 
-						fixedinterestVal, graceVal, graceInterest, 
-						gracePeriodVal);
+				graceInterest = loanobj.computeGraceInterest(principal, interest, fixedinterest, 
+						graceVal, gracePeriod);
+				
+				monthlyPayment = loanobj.computePayment(principal, interest, period, 
+						fixedinterest, graceVal, String.valueOf(graceInterest), 
+						gracePeriod);
+				
+				target = "/Results.jspx";
+				error = false;
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+//				e.printStackTrace();
+				
+				error = true;
+				errorMsg = e.getMessage();
+//				System.out.println(errorMsg);
+				graceInterest = 0.0;
 				monthlyPayment = 0;
+				target = "/UI.jspx";
+				
 			}
+
 			request.getSession().setAttribute("savedPrincipal", principal);
 			request.getSession().setAttribute("savedInterest", interest);
 			request.getSession().setAttribute("savedPeriod", period);
 			request.getSession().setAttribute("savedchecked", grace);
+			request.getSession().setAttribute("error", error);
+			request.getSession().setAttribute("errorMsg", errorMsg);
 
 		
 			
@@ -138,9 +154,10 @@ public class Start extends HttpServlet {
 			request.getSession().setAttribute("gracePerInt", graceInterest);
 			request.getSession().setAttribute("monthlyPay", monthlyPayment);
 			
-			target = "/Results.jspx";
+			
 		}else {
 			if(!resetQuery) {
+				error = false;
 				request.getSession().setAttribute("savedPrincipal", 0);
 				request.getSession().setAttribute("savedInterest", 0.0);
 				request.getSession().setAttribute("savedPeriod", 0);
--- Lab3/studentCalc3/src/model/Loan.java
+++ Lab3/studentCalc3/src/model/Loan.java
@@ -7,36 +7,115 @@ public class Loan {
 		
 	}
 
-	public static double computePayment(double principal, double interest, double period, 
-			double fixedInterest, boolean grace, double graceInterest, 
-			double gracePeriod ) throws Exception {
+	public double computePayment(String principal, String interest, String period, 
+			String fixedInterest, boolean grace, String graceInterest, 
+			String gracePeriod ) throws Exception {
+		
+		double principalVal;
+		double interestVal;
+		double periodVal;
+		double fixedInterestVal;
+		double gracePeriodVal;
+		double graceInterestVal;
+		try {
+			fixedInterestVal = Double.parseDouble(fixedInterest);
+			gracePeriodVal =  Double.parseDouble(gracePeriod);
+			graceInterestVal = Double.parseDouble(graceInterest);
+			if(fixedInterestVal < 0 | gracePeriodVal < 0 | graceInterestVal < 0) {
+				throw new Exception("IDK WHAT HAPPENED VALUES OUT OF YOUR CONTROL ARE ZEROOOOOOOO!");
+			}
+		} catch(Exception e) {
+			throw new Exception("IDK WHAT HAPPENED FAILED TO PARSE STRINGS OUT OF YOUR CONTROL");
+		}
+		
+		try {
+			principalVal = Double.parseDouble(principal);
+
+		} catch(Exception e) {
+			throw new Exception("Principal must be a valid number. (cannot be left blank)");
+		}
+		
+		try {
+			interestVal = Double.parseDouble(interest);
+		} catch(Exception e) {
+			throw new Exception("Interest must be a valid number. (cannot be left blank)");
+		}
+		
+		try {
+			periodVal =  Double.parseDouble(period);
+		} catch(Exception e) {
+			throw new Exception("Period must be a valid number. (cannot be left blank)");
+		}
+		//validate inputs
+		if(principalVal <= 0) {
+			throw new Exception("Principal must be greater than zero!");
+		}
+		if(interestVal <= 0) {
+			throw new Exception("Interest must be greater than zero!");
+		}
+		if(periodVal <= 0 ) {
+			throw new Exception("Period must be greater than zero!");
+		}
+		
+		
+		
 		double monthlyPay = 0;
-		interest = interest/100;
-		fixedInterest = fixedInterest/100;
+		interestVal = interestVal/100;
+		fixedInterestVal = fixedInterestVal/100;
 		//standard calculation
-		monthlyPay = (interest/12) * ( principal / (1 -  Math.pow((1 + (interest/12)), (-1 * period))));
+		monthlyPay = (interestVal/12) * ( principalVal / (1 -  Math.pow((1 + (interestVal/12)), (-1 * periodVal))));
 		
 		//Advanced calculation
 		if(grace) {
-			monthlyPay = monthlyPay + (graceInterest/gracePeriod);
+			monthlyPay = monthlyPay + (graceInterestVal/gracePeriodVal);
 		}
+		
 	
 		return monthlyPay;
 	}
 	
 	
-	public static double computeGraceInterest(double principal, double interest, double fixedInterest, 
-			boolean grace, double gracePeriod ) throws Exception {
-//		System.out.println(principal);
-//		System.out.println(interest);
-//		System.out.println(fixedInterest);
-//		System.out.println(grace);
-//		System.out.println(gracePeriod);
+	public double computeGraceInterest(String principal, String interest, String fixedInterest, 
+			boolean grace, String gracePeriod ) throws Exception {
+		double principalVal;
+		double interestVal;
+		double fixedInterestVal;
+		double gracePeriodVal;
+		try {
+			fixedInterestVal = Double.parseDouble(fixedInterest);
+			gracePeriodVal =  Double.parseDouble(gracePeriod);
+			if(fixedInterestVal < 0 | gracePeriodVal < 0) {
+				throw new Exception("IDK WHAT HAPPENED VALUES OUT OF YOUR CONTROL ARE ZEROOOOOOOO!");
+			}
+		} catch(Exception e) {
+			throw new Exception("IDK WHAT HAPPENED FAILED TO PARSE STRINGS OUT OF YOUR CONTROL");
+		}
+		
+		try {
+			principalVal = Double.parseDouble(principal);
+
+		} catch(Exception e) {
+			throw new Exception("Principal must be a valid number. (cannot be left blank)");
+		}
+		
+		try {
+			interestVal = Double.parseDouble(interest);
+		} catch(Exception e) {
+			throw new Exception("Interest must be a valid number. (cannot be left blank)");
+		}
+		
+		//validate inputs
+		if(principalVal <= 0) {
+			throw new Exception("Principal must be greater than zero!");
+		}
+		if(interestVal <= 0) {
+			throw new Exception("Interest must be greater than zero!");
+		}
 		double graceInterest = 0;
-		interest = interest/100;
-		fixedInterest = fixedInterest/100;
+		interestVal = interestVal/100;
+		fixedInterestVal = fixedInterestVal/100;
 		if(grace) {
-			graceInterest = principal * ((interest + fixedInterest)/12) * gracePeriod;
+			graceInterest = principalVal * ((interestVal + fixedInterestVal)/12) * gracePeriodVal;
 		}
 		return graceInterest;
 	}
