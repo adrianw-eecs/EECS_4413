4540ac58c0b677162c0fff1e8d06849c943ffaa3
Adr ADR <adrianw@my.yorku.ca>
2019-02-05 18:11:49 -0500

    fixed some errors with javascript and Ajax

--- Lab4/studentCalc4/WebContent/res/mc.js
+++ Lab4/studentCalc4/WebContent/res/mc.js
@@ -1,18 +1,21 @@
 function doSimpleAjax(address){
-	var xhr = new XMLHttpRequest();
-
-	buildString = "principalone=" + document.getElementById("principalone").value;
-	buildString = buildString + "&" + "interestone=" + document.getElementById("interestone").value;
-	buildString = buildString + "&" + "periodone=" + document.getElementById("periodone").value;
-	buildString = buildString + "&" + "graceP=" + document.getElementById("grace").checked;
-
-	alert(buildString);
-	xhr.open("GET", (address + "?" + buildString), true);
-	xhr.onreadystatechange = function () {
-		handler(xhr);
-	};
-
-	xhr.send();
+	if (validate()){
+			var xhr = new XMLHttpRequest();
+	
+			buildString = "principalone=" + document.getElementById("principalone").value;
+			buildString = buildString + "&" + "interestone=" + document.getElementById("interestone").value;
+			buildString = buildString + "&" + "periodone=" + document.getElementById("periodone").value;
+			buildString = buildString + "&" + "graceP=" + document.getElementById("grace").checked;
+	
+//			alert(buildString);	
+			xhr.open("GET", (address + "?" + buildString), true);
+			xhr.onreadystatechange = function () {
+				handler(xhr);
+			};
+	
+			xhr.send();
+		}
+	
 }
 
 function handler(xhr){
@@ -23,19 +26,20 @@ function handler(xhr){
 }
 
 function validate(){
-	validatePrincipal()
-	validateInterest()
-	validatePeriod()
+	return validatePrincipal() && validateInterest() && validatePeriod()
 }
 
 function validatePrincipal(){
 	p = document.getElementById("principalone").value;
 	if(isNaN(p) || p <= 0){
 		alert("Principal is invalid!");
-		document.getElementById("principalError").innerHTML = "Principal is invalid!";
+		document.getElementById("principaltxt").innerHTML = "Principal: *";
+		document.getElementById("principaltxt").style.color="Red";
+		//
 		return false;
 	} else {
-		document.getElementById("principalError").innerHTML = "";
+		document.getElementById("principaltxt").innerHTML = "Principal: ";
+		document.getElementById("principaltxt").style.color="#09C";
 		return true;
 	}
 }
@@ -44,10 +48,13 @@ function validateInterest(){
 	p = document.getElementById("interestone").value;
 	if(isNaN(p) || p <= 0 || p >= 100){
 		alert("Interest is invalid! Must be between 0 and 100");
-		document.getElementById("interestError").innerHTML = "Interest is invalid! Must be between 0 and 100";
+		document.getElementById("interesttxt").innerHTML = "Interest: *";
+		document.getElementById("interesttxt").style.color="Red";
+		//
 		return false;
 	} else {
-		document.getElementById("interestError").innerHTML = "";
+		document.getElementById("interesttxt").innerHTML = "Interest: ";
+		document.getElementById("interesttxt").style.color="#09C";
 		return true;
 	}
 }
@@ -55,10 +62,13 @@ function validatePeriod(){
 	p = document.getElementById("periodone").value;
 	if(isNaN(p) || p <= 0){
 		alert("Period is invalid! Must be greater 0");
-		document.getElementById("periodError").innerHTML = "Period is invalid! Must be greater 0";
+		document.getElementById("periodtxt").innerHTML = "Period: *";
+		document.getElementById("periodtxt").style.color="Red";
+		//
 		return false;
 	} else {
-		document.getElementById("periodError").innerHTML = "";
+		document.getElementById("periodtxt").innerHTML = "Period: ";
+		document.getElementById("periodtxt").style.color="#09C";
 		return true;
 	}
 	
